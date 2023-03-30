733679fcdc3f41032ac3375424710db8857c18f2
Renato Angelo <renatoangelo@gmail.com>
2023-03-29 22:21:46 -0300

    Updated

--- index.php
+++ index.php
@@ -16,7 +16,7 @@ if(!empty($data['sendlogin'])) {
 <head>
 <meta charset="UTF-8" />
     
-    <title>WhatsApp ContactLess</title>
+    <title>WhatsApp</title>
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
     <meta http-equiv="X-UA-Compatible" content="ie=edge" />
